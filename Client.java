6a205ecf63d52b8fe70616f0c2142d8b85ae5c02
BancesDiego27 <diego.bances@galileo.edu>
2024-07-15 12:04:35 -0600

    Falta arreglar la IP del host y del Cliente

---
+++ Client.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Client {
+    private static String protocolo;
+    private static String serverIp;
+    private static int puerto;
+
+    public static void main(String[] args) {
+        parseArguments(args);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Ingrese operación matemática (o EXIT para salir): ");
+            String operation = scanner.nextLine();
+
+            if (operation.equalsIgnoreCase("EXIT")) {
+                sendExitMessage();
+                break;
+            }
+
+            try {
+
+                if (protocolo.equalsIgnoreCase("TCP")) {
+                    conectarTCP(operation);
+                } else if(protocolo.equalsIgnoreCase("UDP")) {
+                    conectarUDP(operation);
+                }
+                else{
+                    System.err.println("Error: Ingrese un Protocolo Valido TCP / UDP");
+                }
+    
+
+            } catch (Exception e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+        scanner.close();
+    }
+
+    private static void parseArguments(String[] args) {
+        protocolo= "TCP"; // Si no trae nada en los argumentos se establecen valores por defecto no se especifican asi que asumire TCP y el puerto 3001
+        puerto = 3001;
+        for (int i = 0; i < args.length; i += 2) {
+            String key = args[i];
+            String value = args[i + 1];
+            switch (key.toLowerCase()) {
+                case "protocol":
+                    protocolo = value;
+                    break;
+                case "server":
+                    serverIp = value;
+                    break;
+                case "port":
+                    puerto = Integer.parseInt(value);
+                    break;
+            }
+        }
+    }
+
+    private static String conectarTCP(String operation) throws IOException {
+        Socket socket = new Socket(serverIp, puerto);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        out.println(operation);
+        imprimirMensaje("<", "client", "TCP", "request", operation);
+
+        String response = in.readLine();
+        imprimirMensaje(">", "server", "TCP", "response", response);
+
+        socket.close();
+        return response;
+    }
+
+    private static String conectarUDP(String operation) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress address = InetAddress.getByName(serverIp);
+        byte[] sendBuffer = operation.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, address, puerto);
+        socket.send(sendPacket);
+        imprimirMensaje("<", "client", "UDP", "request", operation);
+
+        byte[] receiveBuffer = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        socket.receive(receivePacket);
+        String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        imprimirMensaje(">", "server", "UDP", "response", response);
+
+        socket.close();
+        return response;
+    }
+
+    private static void sendExitMessage() {
+        try {
+            if (protocolo.equalsIgnoreCase("TCP")) {
+                conectarTCP("EXIT");
+            } else {
+                conectarUDP("EXIT");
+            }
+        } catch (IOException e) {
+            System.err.println("Error sending EXIT message: " + e.getMessage());
+        }
+    }
+
+    private static void imprimirMensaje(String direccion, String hostType, String protocolo, String descripcion, String mensaje) {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String localIP = inetAddress.getHostAddress();
+            String host = hostType.equals("client") ? localIP : serverIp;
+            String dateTime = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+            System.out.println(String.format("[%s] %s %s [%s] %s: %s", direccion, host, hostType, dateTime, protocolo, descripcion + ": " + mensaje));
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,150 @@
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Server {
+    private static String protocolo;
+    private static int puerto;
+
+    public static void main(String[] args) {
+        parseArguments(args);
+
+        try {
+            if (protocolo.equalsIgnoreCase("TCP")) {
+                inciarTCPServer();
+            } else if(protocolo.equalsIgnoreCase("UDP")) {
+                iniciarUDPServer();
+            }
+            else{
+                System.err.println("Error: Ingrese un Protocolo Valido TCP / UDP");
+            }
+
+            
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static void parseArguments(String[] args) {
+        protocolo= "TCP"; // Si no trae nada en los argumentos se establecen valores por defecto no se especifican asi que asumire TCP y el puerto 3001
+        puerto = 3001;
+        for (int i = 0; i < args.length; i += 2) {
+
+            String key = args[i]; // Agarrando el valor de protocol / port
+            String value = args[i + 1]; // Cual es el valor de la llave  
+            switch (key.toLowerCase()) {
+                case "protocol":
+                    protocolo = value;
+                    break;
+                case "port":
+                    puerto = Integer.parseInt(value) ;
+                    break;
+            }
+        }
+    }
+
+    private static void inciarTCPServer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(puerto);
+        System.out.println("TCP server iniciado en el puerto " + puerto);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Leer lo que escribe el cliente
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // Manda mi resultado al cliente
+            System.out.println(clientSocket.getInetAddress().getHostAddress());
+            String request = in.readLine(); // La peticion del usuario
+            String clientIP = clientSocket.getInetAddress().getHostAddress();
+            imprimirMensaje(">", "client", "TCP", "request", request,clientIP);
+
+            if (request.equalsIgnoreCase("EXIT")) { // Solo se deberia de cerrar conexion con el cliente si ponen EXIT
+                clientSocket.close();
+                break;
+            }
+
+            String response = hacerOperacion(request); // la funcion analiza la operacion y devuelve un resultado
+            out.println(response);
+            imprimirMensaje("<", "server", "TCP", "response", response,clientIP);
+
+           // clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    private static void iniciarUDPServer() throws IOException {
+        DatagramSocket socket = new DatagramSocket(puerto);
+        System.out.println("UDP server iniciado en el puerto  " + puerto);
+
+        byte[] receiveBuffer = new byte[1024];
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length); // Recibe la info del cliente 
+            socket.receive(receivePacket);
+
+            String request = new String(receivePacket.getData(), 0, receivePacket.getLength()); // La peticion del usuario
+            imprimirMensaje(">", "client", "UDP", "request", request, "12");
+
+            if (request.equalsIgnoreCase("EXIT")) {  // Solo se deberia de cerrar conexion con el cliente si ponen EXIT
+                break;
+            }
+
+            String response = hacerOperacion(request); // la funcion analiza la operacion y devuelve un resultado
+            byte[] sendBuffer = response.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, receivePacket.getAddress(), receivePacket.getPort());
+            socket.send(sendPacket); // manda al cliente el resultado de la operacion 
+            imprimirMensaje("<", "server", "UDP", "response", response,"12");
+        }
+        socket.close();
+    }
+
+    private static String hacerOperacion(String operation) {
+        try {
+            String[] parts = operation.split(" "); // Deberian de tener un formato Digito Signo Digito  ej 1 + 2 , 5 - 4, 9 * 8, 7 / 3 , 8 % 2
+            if (parts.length != 3) throw new IllegalArgumentException("Operación inválida");
+
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[2]);
+            String operator = parts[1];
+
+            int result;
+            switch (operator) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "%":
+                    result = a % b;
+                    break;
+                case "EXIT":
+                    result = -500;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Operador inválido");
+            }
+            return String.valueOf(result);
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    private static void imprimirMensaje(String direccion, String hostType, String protocolo, String descripcion, String mensaje , String clientIP) {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String localIP = inetAddress.getHostAddress();
+            String host = hostType.equals("server") ? localIP : clientIP;
+            String dateTime = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+            System.out.println(String.format("[%s] %s %s [%s] %s: %s", direccion, host, hostType, dateTime, protocolo, descripcion + ": " + mensaje));
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
